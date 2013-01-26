2f6273bdbd76089418925a7333b974a3fe5a39fb
irmen <irmen@08cfa0ad-1c00-4aa0-9110-f4b10de7e716>
2013-01-26 02:27:24 +0000

    release build

--- dotnet/build-dotnet-microsoft.cmd
+++ dotnet/build-dotnet-microsoft.cmd
@@ -1,4 +1,3 @@
 echo "Compiling .net source (with microsoft windows sdk msbuild)"
-msbuild /verbosity:minimal /p:Platform="Any CPU" /p:Configuration="Debug" Serpent.sln /t:Rebuild
-copy Serpent\bin\Debug\*.dll build\
-copy Serpent\bin\Debug\*.pdb build\
+msbuild /verbosity:minimal /p:Platform="Any CPU" /p:Configuration="Release" Serpent.sln /t:Rebuild
+copy Serpent\bin\Release\*.dll build\
