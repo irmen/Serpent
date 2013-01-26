d3514ae8db71cdc16af4bdd54a94ed020981e848
irmen <irmen@08cfa0ad-1c00-4aa0-9110-f4b10de7e716>
2013-01-26 15:47:55 +0000

    path fix

--- dotnet/test-dotnet.sh
+++ dotnet/test-dotnet.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Building..."
-. build-dotnet-mono.sh
+. ./build-dotnet-mono.sh
 
 echo "Running tests"
 nunit-console -noshadow -nothread Serpent.Test/bin/Release/Razorvine.Serpent.Test.dll
