c31985f070d9828a32337c7921c4dc4c2e623011
irmen <irmen@08cfa0ad-1c00-4aa0-9110-f4b10de7e716>
2013-03-30 21:16:40 +0000

    added c# example

---
+++ dotnet/Serpent.Test/Example.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+using NUnit.Framework;
+using Razorvine.Serpent.Parsing;
+using Razorvine.Serpent.Serializing;
+
+namespace Razorvine.Serpent.Test
+{
+	/// <summary>
+	/// Example usage.
+	/// </summary>
+	/// 
+	[TestFixture]
+	public class Example 
+	{
+		[Test]
+		[Ignore("example")]
+		public void ExampleUsage()
+		{
+			var data = new Dictionary<string, object> {
+				{"tuple", new int[] { 1,2,3 } },
+				{"date", DateTime.Now},
+				{"set", new HashSet<string> { "a", "b", "c" } }
+			};
+			
+			// serialize data structure to bytes
+			Serializer serpent = new Serializer(indent: true);
+			byte[] ser = serpent.Serialize(data);
+			Console.WriteLine("Serialized:");
+			Console.WriteLine(Encoding.UTF8.GetString(ser));
+			
+			// parse the serialized bytes back into an abstract syntax tree of the datastructure
+			Parser parser = new Parser();
+			Ast ast = parser.Parse(ser);
+			Console.WriteLine("\nParsed AST:");
+			Console.WriteLine(ast.Root.ToString());
+			
+			// turn the Ast into regular .net objects
+			var visitor = new ObjectifyVisitor();
+			ast.Accept(visitor);
+			var dict = (IDictionary<object, object>) visitor.GetObject();
+
+			// print the results
+			Console.Write("Tuple items: ");
+			object[] tuple = (object[]) dict["tuple"];
+			Console.WriteLine(string.Join(", ", tuple.Select(e=>e.ToString()).ToArray()));
+			Console.WriteLine("Date: {0}", dict["date"]);
+			Console.Write("Set items: ");
+			HashSet<object> set = (HashSet<object>) dict["set"];
+			Console.WriteLine(string.Join(", ", set.Select(e=>e.ToString()).ToArray()));
+		}
+	}
+}
--- dotnet/Serpent/Properties/AssemblyInfo.cs
+++ dotnet/Serpent/Properties/AssemblyInfo.cs
@@ -9,12 +9,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Serpent")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Razorvine.Serpent")]
+[assembly: AssemblyDescription("Serpent serialization")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Serpent")]
-[assembly: AssemblyCopyright("Copyright 2013")]
+[assembly: AssemblyProduct("Serpent serialization")]
+[assembly: AssemblyCopyright("Copyright 2013 Irmen de Jong")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -28,4 +28,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.7.*")]
+[assembly: AssemblyFileVersion("0.7.*")]
