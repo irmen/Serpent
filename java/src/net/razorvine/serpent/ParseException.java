87fe4c9fec35b7b45bd6bc0109e5d70c91a91991
irmen <irmen@08cfa0ad-1c00-4aa0-9110-f4b10de7e716>
2013-03-30 22:50:22 +0000

    first batch of java

---
+++ java/src/net/razorvine/serpent/ParseException.java
@@ -0,0 +1,37 @@
+/**
+ * Serpent, a Python literal expression serializer/deserializer
+ * (a.k.a. Python's ast.literal_eval in .NET)
+ * Software license: "MIT software license". See http://opensource.org/licenses/MIT
+ * @author Irmen de Jong (irmen@razorvine.net)
+ */
+
+package net.razorvine.serpent;
+
+/**
+ * Exception that is used when a serpent parsing error occurs.
+ */
+public class ParseException extends Exception {
+
+	private static final long serialVersionUID = -667969719712045595L;
+
+	public ParseException() {
+	}
+
+	public ParseException(String message) {
+		super(message);
+	}
+
+	public ParseException(Throwable cause) {
+		super(cause);
+	}
+
+	public ParseException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ParseException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+}
