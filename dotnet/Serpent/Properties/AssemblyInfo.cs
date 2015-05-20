0a0a1d5d726f653dcf8224ef4193917653a6fe47
Irmen de Jong <irmen@razorvine.net>
2015-05-20 22:23:03 +0200

    Added NuGet spec and description. Fixes #10

--- dotnet/Serpent/Properties/AssemblyInfo.cs
+++ dotnet/Serpent/Properties/AssemblyInfo.cs
@@ -10,10 +10,18 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Razorvine.Serpent")]
-[assembly: AssemblyDescription("Serpent serialization")]
+[assembly: AssemblyDescription(@"Serpent Python literal expression serialization.
+
+Serpent provides Python ast.literal_eval() compatible object tree serialization.
+It serializes an object tree into bytes that can be transferred to Python and
+decoded there (using ast.literal_eval()). It can ofcourse also deserialize
+such a Python expression itself, back into the equivalent .NET datatypes.
+
+More info for the Python version is on Pypi: https://pypi.python.org/pypi/serpent
+")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Serpent serialization")]
+[assembly: AssemblyCompany("Irmen de Jong")]
+[assembly: AssemblyProduct("Serpent Python literal expression serialization")]
 [assembly: AssemblyCopyright("Copyright 2015 Irmen de Jong")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
