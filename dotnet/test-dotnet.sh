838ae3a636fcf7e56cd49177af8e44ef34930ce2
Irmen de Jong <irmen@razorvine.net>
2016-01-27 01:51:06 +0100

    nunit command update

--- dotnet/test-dotnet.sh
+++ dotnet/test-dotnet.sh
@@ -3,8 +3,4 @@ echo "Building..."
 . ./build-dotnet-mono.sh
 
 echo "Running tests"
-
-# note: nunit-2.6.2 crashes on Mono. Stick with 2.6.1 for the time being.
-NUNIT="mono ${MONO_OPTIONS} ${HOME}/Projects/NUnit-2.6.1/bin/nunit-console.exe"
-
-${NUNIT} -framework:4.0 -noshadow -nothread Serpent.Test/bin/Release/Razorvine.Serpent.Test.dll
+nunit-console -noshadow Serpent.Test/bin/Release/Razorvine.Serpent.Test.dll
