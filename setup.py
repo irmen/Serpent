0971fcc8de663f572a33cd0de779dddb408e38ff
Irmen de Jong <irmen@razorvine.net>
2017-11-05 00:28:03 +0100

    removed python 3.3 support (it is end of life) added python 3.7 support and tox config (release is approaching)

--- setup.py
+++ setup.py
@@ -163,10 +163,10 @@ When you run this (with python 3.2+) it prints:
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Software Development"
     ],
 
