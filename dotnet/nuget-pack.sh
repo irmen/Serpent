e4d3d7741b4f4b563f790cd258176d67c3b06046
Irmen de Jong <irmen@razorvine.net>
2018-01-25 23:17:38 +0100

    nuget script

--- dotnet/nuget-pack.sh
+++ dotnet/nuget-pack.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 echo "Building and testing..."
-. ./test.sh
+dotnet test -c Release Serpent/Tests
 
-echo "Creating nuget release package..."
-dotnet pack -c Release Serpent
+echo "\n\nCreating nuget release package..."
+dotnet pack -c Release -o $(pwd)/dist Serpent
+
+echo "\n\nPackage available in dist/ directory:"
+ls -l dist
+
+echo "\nIf this is allright, publish to nuget.org with:"
+echo "dotnet nuget push dist/Razorvine.Serpent.xxxxx.nupkg -s https://www.nuget.org -k api_key_from_nuget_org"
