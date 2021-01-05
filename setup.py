f2518a0e63bc5654ef314dc8e6fcce719993fcd3
Irmen de Jong <irmen@razorvine.net>
2021-01-05 21:08:56 +0100

    remove Python 3.5 from support list (it is EOL)

--- setup.py
+++ setup.py
@@ -181,7 +181,6 @@ When you run this it prints:
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
